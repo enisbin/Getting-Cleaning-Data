31ff6851a40b8ef56c2a743ebe939b3193038499
enisbin <nishchay_iift@yahoo.com>
2016-02-05 16:54:42 +0100

    Update run_analysis.R

--- run_analysis.R
+++ run_analysis.R
@@ -1,47 +1,46 @@
-if (!("reshape2" %in% rownames(installed.packages())) ) {
-  print("Please install required package \"reshape2\" before proceeding")
-} else {
-  library(reshape2)
-  activity_labels <- read.table("./activity_labels.txt",col.names=c("activity_id","activity_name"))
-  
-  features <- read.table("features.txt")
-  feature_names <-  features[,2]
-  testdata <- read.table("./test/X_test.txt")
-  colnames(testdata) <- feature_names
-  
-  traindata <- read.table("./train/X_train.txt")
-  colnames(traindata) <- feature_names
-  
-  test_subject_id <- read.table("./test/subject_test.txt")
-  colnames(test_subject_id) <- "subject_id"
-  
-  test_activity_id <- read.table("./test/y_test.txt")
-  colnames(test_activity_id) <- "activity_id"
-  
-  train_subject_id <- read.table("./train/subject_train.txt")
-  colnames(train_subject_id) <- "subject_id"
-  
-  train_activity_id <- read.table("./train/y_train.txt")
-  colnames(train_activity_id) <- "activity_id"
-  
-  test_data <- cbind(test_subject_id , test_activity_id , testdata)
-  
-  train_data <- cbind(train_subject_id , train_activity_id , traindata)
-  
-  all_data <- rbind(train_data,test_data)
-  
-  mean_col_idx <- grep("mean",names(all_data),ignore.case=TRUE)
-  mean_col_names <- names(all_data)[mean_col_idx]
-  std_col_idx <- grep("std",names(all_data),ignore.case=TRUE)
-  std_col_names <- names(all_data)[std_col_idx]
-  meanstddata <-all_data[,c("subject_id","activity_id",mean_col_names,std_col_names)]
-  
-  descrnames <- merge(activity_labels,meanstddata,by.x="activity_id",by.y="activity_id",all=TRUE)
-  
-  data_melt <- melt(descrnames,id=c("activity_id","activity_name","subject_id"))
-  
-  mean_data <- dcast(data_melt,activity_id + activity_name + subject_id ~ variable,mean)
-  
-  write.table(mean_data,"./tidy_movement_data.txt")
-  
+library(reshape2)
+zipfile <- "getdata_dataset.zip"
+if (!file.exists(zipfile)){
+  ziplocation <- "https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip "
+  download.file(ziplocation, zipfile, method="curl")
+}  
+if (!file.exists("UCI HAR Dataset")) { 
+  unzip(zipfile) 
 }
+
+readzipfile1 <- read.table("UCI HAR Dataset/activity_labels.txt")
+readzipfile1[,2] <- as.character(readzipfile1[,2])
+readzipfile2 <- read.table("UCI HAR Dataset/features.txt")
+readzipfile2[,2] <- as.character(readzipfile2[,2])
+
+# Extract only the data on mean and standard deviation
+readzipfile2value <- grep(".*mean.*|.*std.*", readzipfile2[,2])
+readzipfile2value.names <- readzipfile2[readzipfile2value,2]
+readzipfile2value.names = gsub('-mean', 'Mean', readzipfile2value.names)
+readzipfile2value.names = gsub('-std', 'Std', readzipfile2value.names)
+readzipfile2value.names <- gsub('[-()]', '', readzipfile2value.names)
+
+
+# Load the datasets
+loaddata1 <- read.table("UCI HAR Dataset/train/X_train.txt")[readzipfile2value]
+loaddata1value <- read.table("UCI HAR Dataset/train/Y_train.txt")
+loaddata1value2 <- read.table("UCI HAR Dataset/train/subject_train.txt")
+loaddata1 <- cbind(loaddata1value2, loaddata1value, loaddata1)
+
+check <- read.table("UCI HAR Dataset/test/X_test.txt")[readzipfile2value]
+checkactivities <- read.table("UCI HAR Dataset/test/Y_test.txt")
+checksubjects <- read.table("UCI HAR Dataset/test/subject_test.txt")
+check <- cbind(checksubjects, checkactivities, check)
+
+# merge datasets and add labels
+combined <- rbind(loaddata1, check)
+colnames(combined) <- c("subject", "activity", readzipfile2value.names)
+
+# turn activities & subjects into factors
+combined$activity <- factor(combined$activity, levels = readzipfile1[,1], labels = readzipfile1[,2])
+combined$subject <- as.factor(combined$subject)
+
+combined.data <- melt(combined, id = c("subject", "activity"))
+combined.mean <- dcast(combined.data, subject + activity ~ variable, mean)
+
+write.table(combined.mean, "tidy.txt", row.names = FALSE, quote = FALSE)
